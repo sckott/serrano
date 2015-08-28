0ce0755b086662d2afb5c103721b5bb029e049af
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-28 09:01:36 -0700

    changes

--- lib/crossrefrb/request.rb
+++ lib/crossrefrb/request.rb
@@ -1,21 +1,38 @@
 module Crossref
   class Request #:nodoc:
+    include HTTParty
+    debug_output $stdout # <= this is it!
+
+    $crbase = "http://api.crossref.org/"
+
     attr_accessor :endpt
     attr_accessor :doi
+    attr_accessor :query
 
-    def initialize(endpt, doi)
+    def initialize(endpt, doi, query)
       self.endpt = endpt
       self.doi = doi
+      self.query = query
     end
 
     def perform
-      url = "http://api.crossref.org/" + self.endpt
-      coll = []
-      Array(self.doi).each do |x|
-        puts url + '/' + x
-        # coll << HTTParty.get(url + '/' + x)
+      url = $crbase + self.endpt
+
+      args = {"query" => self.query}
+      args = args.delete_if { |k, v| v.nil? }
+
+      if doi.nil?
+        # puts args
+        # puts url
+        res = HTTParty.get(url, :query => args)
+        return res
+      else
+        coll = []
+        Array(self.doi).each do |x|
+          coll << HTTParty.get(url + '/' + x)
+        end
+        return coll
       end
-      return coll
     end
   end
 end
