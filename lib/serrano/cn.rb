6018c9344eb0a4c61d62f07a176308dfffdf8011
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-27 13:37:26 -0700

    tweaks to cn to be a class

--- lib/serrano/cn.rb
+++ lib/serrano/cn.rb
@@ -2,30 +2,29 @@ require "serrano/version"
 require "cnrequest"
 
 ##
-# CN - Content Negotiation
+# ContentNegotiation - Content Negotiation class
 #
 # @see http://www.crosscite.org/cn/ for details
 module Serrano
-  module CN
 
-    ##
-    # Get citations in various formats from CrossRef
-    #
-    # @param ids [String] DOIs
-    # @param format [String] Format
-    # @param style [String] Style
-    # @param locale [String] Locale
-    # @return [Hash] A hash
-    #
-    # @example
-    #     require 'serrano'
-    #     # By default, you get bibtex, apa format, in en-US locale
-    #     Serrano.cn(ids: '10.1126/science.169.3946.635')
-    #     # get citeproc-json
-    #     Serrano.cn(ids: '10.1126/science.169.3946.635', format: "citeproc-json")
-    def self.cn(ids:, format: "bibtex", style: 'apa', locale: "en-US")
-      CNRequest.new(ids, format, style, locale).perform
+  class ContentNegotiation
+
+    attr_accessor :ids
+    attr_accessor :format
+    attr_accessor :style
+    attr_accessor :locale
+
+    def initialize(ids, format = "bibtex", style = "apa", locale = "en-US")
+      self.ids = ids
+      self.format = format
+      self.style = style
+      self.locale = locale
+    end
+
+    def cn
+      CNRequest.new(self.ids, self.format, self.style, self.locale).perform
     end
 
   end
+
 end
--- lib/serrano/cnrequest.rb
+++ lib/serrano/cnrequest.rb
@@ -9,7 +9,7 @@ require 'serrano/helpers/configuration'
 # Serrano::CNRequest
 #
 # Class to perform HTTP requests to the Crossref API
-module CN
+module Serrano
   class CNRequest #:nodoc:
 
     attr_accessor :ids
