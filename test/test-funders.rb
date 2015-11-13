cbb34d58480a4149c8ca28f8590f8df42b9d0ca6
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-12 16:28:02 -0800

    added funders and journals test

---
+++ test/test-funders.rb
@@ -0,0 +1,51 @@
+require 'simplecov'
+SimpleCov.start
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+require "serrano"
+require 'fileutils'
+require "test/unit"
+require "json"
+
+class TestResponse < Test::Unit::TestCase
+
+  def setup
+    @id = 100004410
+    @ids = ['10.13039/100000001','10.13039/100000015']
+  end
+
+  def test_funders
+    res = Serrano.funders(ids: @id)
+    assert_equal(1, res.length)
+    assert_equal(Array, res.class)
+    assert_equal(Hash, res[0].class)
+    assert_equal("funder", res[0]['message-type'])
+  end
+
+  def test_funders_many_ids
+    res = Serrano.funders(ids: @ids)
+    assert_equal(2, res.length)
+    assert_equal(Array, res.class)
+    assert_equal(Hash, res[0].class)
+    assert_equal("funder", res[0]['message-type'])
+    assert_equal("National Science Foundation", res[0]['message']['name'])
+  end
+
+  def test_funders_query
+    res = Serrano.funders(query: "NSF")
+    assert_equal(4, res.length)
+    assert_equal(Hash, res.class)
+    assert_equal("funder-list", res[0]['message-type'])
+  end
+
+  def test_funders_filter_handler
+    res = Serrano.funders(filter: {has_funder: true})
+    assert_equal(Hash, res.class)
+    assert_equal("validation-failure", res['message-type'])
+    assert_equal("filter-not-available", res['message'][0]['type'])
+  end
+
+end
---
+++ test/test-journals.rb
@@ -0,0 +1,52 @@
+require 'simplecov'
+SimpleCov.start
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+require "serrano"
+require 'fileutils'
+require "test/unit"
+require "json"
+
+class TestResponse < Test::Unit::TestCase
+
+  def setup
+    @id = "2167-8359"
+    @ids = ['1803-2427', '2326-4225']
+  end
+
+  def test_journals
+    res = Serrano.journals(ids: @id)
+    assert_equal(1, res.length)
+    assert_equal(Array, res.class)
+    assert_equal(Hash, res[0].class)
+    assert_equal("journal", res[0]['message-type'])
+  end
+
+  def test_journals_many_dois
+    res = Serrano.journals(ids: @ids)
+    assert_equal(2, res.length)
+    assert_equal(Array, res.class)
+    assert_equal(Hash, res[0].class)
+    assert_equal("journal", res[0]['message-type'])
+    assert_equal("journal", res[1]['message-type'])
+  end
+
+  def test_journals_query
+    res = Serrano.journals(query: "ecology")
+    assert_equal(4, res.length)
+    assert_equal(Hash, res.class)
+    assert_equal("journal-list", res['message-type'])
+    assert_equal(String, res['message']['items'][0]['title'])
+  end
+
+  def test_journals_filter_handler
+    res = Serrano.journals(ids: "2167-8359", works: false, filter: {from_pub_date: '2014-03-03'})
+    assert_equal(Array, res.class)
+    assert_equal("validation-failure", res[0]['message-type'])
+    assert_equal("filter-not-available", res[0]['message'][0]['type'])
+  end
+
+end
