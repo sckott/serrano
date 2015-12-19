7c4fd0c5b31fe52d231237d72d6aeec4d80547f7
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-18 18:13:42 -0800

    added licenses test

---
+++ test/test-licenses.rb
@@ -0,0 +1,36 @@
+require 'simplecov'
+SimpleCov.start
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+require "serrano"
+require 'fileutils'
+require "test/unit"
+require "json"
+
+class TestLicenses < Test::Unit::TestCase
+
+  def test_licenses
+    res = Serrano.licenses()
+    assert_equal(4, res.length)
+    assert_equal(Hash, res.class)
+    assert_equal("license-list", res['message-type'])
+    assert_true(res['message']['items'].length > 100)
+  end
+
+  def test_licenses_query
+    res = Serrano.licenses(query: "creative")
+    assert_equal(4, res.length)
+    assert_equal(Hash, res.class)
+    assert_true(res['message']['items'].length < 50)
+    assert_equal(MatchData, res['message']['items'][0]['URL'].match('creative').class)
+  end
+
+  def test_licenses_limit
+    # limit doesn't work on this route
+    res = Serrano.licenses(limit: 3);
+    assert_true(res['message']['items'].length > 100)
+  end
+end
