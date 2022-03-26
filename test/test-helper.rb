0d7510177fb2ec92bdae705dd353d6fe57ab2f3e
Scott Chamberlain <sckott@fastmail.com>
2022-03-25 21:35:27 -0700

    standard formatting fixes

--- test/test-helper.rb
+++ test/test-helper.rb
@@ -12,7 +12,7 @@ require "vcr"
 VCR.configure do |c|
   c.cassette_library_dir = "test/vcr_cassettes"
   c.hook_into :webmock
-  c.filter_sensitive_data('<email>') { ENV['CROSSREF_EMAIL'] }
+  c.filter_sensitive_data("<email>") { ENV["CROSSREF_EMAIL"] }
 end
 
 require "test/unit"
