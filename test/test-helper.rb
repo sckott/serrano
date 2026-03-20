e1faf4a12915ee9268328f5ee454dcb0dbc3da25
Scott Chamberlain <sckott@fastmail.com>
2026-03-20 16:12:40 -0700

    add hook for ignoring vcr cassettes during rake test

--- test/test-helper.rb
+++ test/test-helper.rb
@@ -1,8 +1,15 @@
 require "vcr"
+require "webmock"
+
 VCR.configure do |c|
   c.cassette_library_dir = "test/vcr_cassettes"
   c.hook_into :webmock
   c.filter_sensitive_data("<email>") { ENV["CROSSREF_EMAIL"] }
+end
+
+if ENV["DISABLE_VCR"]
+  VCR.turn_off!(ignore_cassettes: true)
+  WebMock.allow_net_connect!
 end
 
 require "test/unit"
